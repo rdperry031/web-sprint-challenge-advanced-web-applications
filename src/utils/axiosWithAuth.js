9451a5df8059d0d0cb03ea51fe47e5cccba13d61
rdperry031 <rdperry031@gmail.com>
2021-10-15 11:36:15 -0500

    completed axiosWithAuth utility

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 const axiosWithAuth = ()=> {
-    
+    const token = window.localStorage.getItem('token')
+
+    return axios.create({
+        headers: {
+            Authorization: token
+        }
+    })
 }
 
 export default axiosWithAuth;
